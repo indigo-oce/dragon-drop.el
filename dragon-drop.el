f423563417fa916b51495683aa1913ce9d4cefc9
indigo-oce <indigoschulze@gmail.com>
2022-07-09 00:05:21 +0800

    add dragon-drop vars & fn

--- dragon-drop.el
+++ dragon-drop.el
@@ -20,11 +20,37 @@
 
 ;;; Commentary:
 
-;; 
+;; Requires 'dragon' cli program, 'dragon-drop' on arch linux.
 
 ;;; Code:
 
+(defvar dragon-drop-program-name "dragon-drop"
+  "Name of the cli program (differs on different distros)")
 
+(defvar dragon-drop-default-args "--all --and-exit")
+(defvar dragon-drop-always-args " --on-top ") ; surrounded by spaces so concating is easier
+
+(defvar dragon-drop-thumb-size 96
+  "Default size of thumbnails (96 is default without the argument)")
+
+(defun dragon-drop (&optional files args thumb-size)
+  "Runs `dragon-drop-program-name' on the selected buffer or the dired marked files.
+By default it will send all files at once and exit (--all --and-exit).
+
+With ARG it will stay until the user exits (with 'q').
+
+Runs shell command the same way as `dired-do-shell-command' does (for file name parsing)."
+  (interactive
+   (list (if (eq major-mode 'dired-mode) (dired-get-marked-files t)
+           (list (or (buffer-file-name)
+                     (error "No File Selected"))))
+         (cond ((equal current-prefix-arg '(4))
+                dragon-drop-always-args)
+               (t (concat dragon-drop-default-args dragon-drop-always-args)))
+         dragon-drop-thumb-size))
+  (setq command (format "%s -s %s %s &" dragon-drop-program-name
+                        thumb-size args))
+  (dired-run-shell-command (dired-shell-stuff-it command files nil nil)))
 
 (provide 'dragon-drop)
 ;;; dragon-drop.el ends here
